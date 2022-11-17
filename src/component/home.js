3e16ce9e2e7b8f0245533d83dd1ae6441cc83728
indra-11 <galunggungone@gmail.com>
2022-11-17 08:45:26 +0700

    changes

--- src/component/home.js
+++ src/component/home.js
@@ -1,10 +1,18 @@
 const Home = () => {
     return (
-        <div className="bg-one min-h-screen flex items-center pt-12 sm:pt-6">
-            <div className="max-w-6xl mx-auto px-5 py-9 sm:py-12">
+        <div className="bg-one min-h-screen flex items-center pt-9 sm:pt-6">
+            <div className="max-w-6xl mx-auto px-5 py-9 sm:py-12 grid gap-4 sm:gap-5 lg:gap-6">
+                <div className='flex'>
+                <img className='w-6 sm:w-8 lg:w-10' src={require('../assets/react.svg').default}  alt='React JS' />
+                    <div className='w-2 sm:w-3 lg:w-4 bg-one rotate-12 -mx-1 sm:-mx-1.5 lg:-mx-2' />
+                    <img className='w-6 sm:w-8 lg:w-10' src={require('../assets/next.svg').default}  alt='Next JS' />
+                    <div className='w-2 sm:w-3 lg:w-4 bg-one rotate-12 -mx-1 sm:-mx-1.5 lg:-mx-2' />
+                    
+                    <img className='w-6 sm:w-8 lg:w-10' src={require('../assets/tailwind.svg').default}  alt='Tailwind CSS' />
+                </div>
                 <h1 className='text-three text-3xl sm:text-4xl lg:text-5xl w-max font-bold'>Hi, I'm Indra</h1>
-                <hr className="border-t-2 border-four w-2/6 mt-5" />
-                <p className="text-two text-lg md:text-xl lg:text-2xl mt-5 mb-9 font-normal">
+                <hr className="border-t-2 border-four w-2/6" />
+                <p className="text-two text-lg md:text-xl lg:text-2xl font-normal">
                     My full name is Muhammad Laksmana Indra.
                     I'm a Frontend developer with React JS, Next JS, Tailwind CSS, and several other technologies.
                     I'm also familiar with some backend technologies like Laravel and MySQL.
@@ -12,7 +20,7 @@ const Home = () => {
                     I'm interested in art and logic, that's why I chose Frontend.
                     I have worked on many projects and have several certificates.
                 </p>
-                <hr className="border-t-2 border-four w-2/6 mb-5 mr-0 ml-auto" />
+                <hr className="border-t-2 border-four w-2/6 mr-0 ml-auto" />
                 <a href='https://www.linkedin.com/in/muhammad-laksmana-indra-3913b422b' target="_blank" rel="noopener noreferrer"
                 className="rounded bg-three text-one text-md sm:text-lg lg:text-xl w-max py-3 px-5 flex items-center mr-0 ml-auto">
                     <i className="fab fa-linkedin-in mr-2" /> Connect
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,29 +1,25 @@
 const Navbar = () => {
+    const Part = ({url, value}) => {
+        return (
+            <li className="hover:text-two cursor-pointer">
+                <a href={url} target="_blank" rel="noopener noreferrer">{value}</a>
+            </li>
+        );
+    }
+
     return (
         <div className='bg-one text-three w-full fixed top-0 z-10 shadow-[0px_1px_10px_-5px_#000000]'>
             <div className="max-w-7xl flex justify-between items-center mx-auto py-3 px-5 text-sm sm:text-base">
                 <h1 className="font-bold">Madanadra.</h1>
                 <ul className="hidden sm:flex gap-5 font-semibold">
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://www.linkedin.com/in/muhammad-laksmana-indra-3913b422b' target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                    </li>
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://github.com/madanadra' target="_blank" rel="noopener noreferrer">GitHub</a>
-                    </li>
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://mail.google.com/mail/?view=cm&fs=1&to=indrafrontend@gmail.com' target="_blank" rel="noopener noreferrer">Contact</a>
-                    </li>
+                    <Part url='https://www.linkedin.com/in/muhammad-laksmana-indra-3913b422b' value='LinkedIn' />
+                    <Part url='https://github.com/madanadra' value='GitHub' />
+                    <Part url='https://mail.google.com/mail/?view=cm&fs=1&to=indrafrontend@gmail.com' value='Contact' />
                 </ul>
                 <ul className="flex sm:hidden gap-5 font-semibold">
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://www.linkedin.com/in/muhammad-laksmana-indra-3913b422b' target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in" /></a>
-                    </li>
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://github.com/madanadra' target="_blank" rel="noopener noreferrer"><i className="fab fa-github" /></a>
-                    </li>
-                    <li className="hover:text-two cursor-pointer">
-                        <a href='https://mail.google.com/mail/?view=cm&fs=1&to=indrafrontend@gmail.com' target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope"></i></a>
-                    </li>
+                    <Part url='https://www.linkedin.com/in/muhammad-laksmana-indra-3913b422b' value={<i className="fab fa-linkedin-in" />} />
+                    <Part url='https://github.com/madanadra' value={<i className="fab fa-github" />} />
+                    <Part url='https://mail.google.com/mail/?view=cm&fs=1&to=indrafrontend@gmail.com' value={<i className="fas fa-envelope"></i>} />
                 </ul>
             </div>
         </div>
