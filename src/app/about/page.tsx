9e5bbb11fa4de0852cfa2d90ee1a3843b1d3b333
madanadra <mlaksindra@gmail.com>
2023-07-31 19:32:04 +0700

    final

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         <div className="grid gap-y-7 sm:w-2/3 px-5 sm:px-20">
             <h1 className="font-bold text-[2em]">A bit about me</h1>
             <div className='grid gap-y-5 text-[1.5em] font-light leading-relaxed'>
-                <h1>I'm Muhammad Laksmana Indra, a Frontend developer with React JS. 
+                <h1>I&apos;m Muhammad Laksmana Indra, a Frontend developer with React JS. 
                 I translate the design into a fast and stable website.</h1>
                 <h1>Besides React JS, I also use Next JS, Tailwind CSS, Bootstrap, Material UI, and other tools. 
                 Familiar with backends like Laravel and MySQL.</h1>
